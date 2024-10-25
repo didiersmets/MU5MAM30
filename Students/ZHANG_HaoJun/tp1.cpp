7b06855291116b3d53877c43c2de92c4b865ebe2
ZHANG_HaoJun <neroued@gmail.com>
2024-10-25 17:05:03 +0200

    tp1 first class

---
+++ Students/ZHANG_HaoJun/tp1.cpp
@@ -0,0 +1,383 @@
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* The goal of this TP is to get acquainted with C (not yet C++)
+ * and at the same time solve the equation
+ *
+ *    - \Delta u + u = f
+ *
+ * on (e.g.) the sphere S^2. The sphere is handy for testing because
+ * "we" know explicit solutions (besides constants) for some particular
+ * cases of f (which ones ?), but process would work on an arbitrary surface
+ * mesh.
+ *
+ * Detailed instructions shall be given on the blackboard prior to the class.
+ * Depending on their background in both programming and the FEM, students will
+ * have the choice to either :
+ * 1) follow me coding/explaining live, and ask questions
+ * 2) fill the gaps by themselves, following the proposed structure
+ * 3) provide some conceptual (algorithmmic/performance/robustness) improvements
+ * or a mix of these...
+ */
+
+/* The C structures we define for our needs */
+
+/* A Vertex is simply a point in R^3 */
+struct Vertex {
+	double x;
+	double y;
+	double z;
+};
+
+/* A triangle refers to three vertices by their index */
+struct Triangle {
+	int a;
+	int b;
+	int c;
+};
+
+/* A mesh is an array of vertices, and an array of triangles
+ * built over these vertices
+ */
+struct Mesh {
+	int vtx_count;
+	int tri_count;
+	struct Vertex *vertices;
+	struct Triangle *triangles;
+};
+
+/* A coefficient of a sparse matrix */
+struct Coeff {
+	int i;
+	int j;
+	double val;
+};
+
+/* A Sparse matrix is an (order independent) array of coeffs,
+ * the non zero entries of the matrix.
+ * That for is called a COO sparse matrixe (COO for coordinates).
+ * It is the simplest form and not the most efficient.
+ * In the main repo we use a (probably) better approach.
+ */
+struct SparseMatrix {
+	int rows;
+	int cols;
+	int nnz; // number of non zero elems
+	struct Coeff *coeffs;
+};
+
+/* A Vector in R^3. Similar to Vertex, but as in affine geometry we may
+ * wish to distinguish vectors and points.
+ */
+struct Vector {
+	double x;
+	double y;
+	double z;
+};
+
+/******************************************************************************
+ * Vectors in R^3
+ ******************************************************************************/
+
+/* Returns a vector from its end points */
+struct Vector vector(struct Vertex A, struct Vertex B)
+{
+	struct Vector V =  {B.x - A.x,
+				  		B.y - A.y,
+				  		B.z - A.z};
+	return V;
+}
+
+double dot(struct Vector A, struct Vector B)
+{
+	return B.x * A.x + B.y * A.y + B.z * A.z;
+}
+
+double norm(struct Vector V) { return sqrt(dot(V, V)); };
+
+
+struct Vector cross(struct Vector A, struct Vector B)
+{
+	struct vector V =  {A.y*B.z - A.z*B.y,
+						A.z*B.x - A.x*B.z,
+						A.x*B.y - A.y*B.x};
+	return V;
+}
+/*****************************************************************************/
+
+/******************************************************************************
+ * Computes the product M * v when M is a SparseMatrix
+ *****************************************************************************/
+void matrix_vector_product(const struct SparseMatrix *M, const double *v, double *Mv)
+{
+	for (int i = 0; i < M->rows; ++i)
+	{
+		Mv[i] = 0;
+	}
+
+	int i, j;
+	for (int k = 0; k < M->nnz; ++k)
+	{
+		i = M->coeffs[k].i;
+		j = M->coeffs[k].j;
+		assert(i < M->rows);
+		assert(j < M->cols);
+		Mv[i] += M->coeffs[k].val * v[j]; 
+	}
+	return;
+}
+
+/******************************************************************************
+ * Builds the P1 stiffness and mass matrices of a given mesh.
+ * We do not try to assemble different elements together here for simplicity.
+ * Both matrices M and S will therefore have 9 * number of triangles.
+ * We derived the formulas in the first lecture.
+ *****************************************************************************/
+void build_fem_matrices(const struct Mesh *m, struct SparseMatrix *S, struct SparseMatrix *M)
+{
+	int N = m->vtx_count;
+	S->rows = S->cols = M->rows = M->cols = m->vtx_count;
+	/* We do not try to assemble, so 9 coeffs per triangle */
+	S->nnz = M->nnz = 9 * m->tri_count;
+	S->coeffs = (struct Coeff *)malloc(S->nnz * sizeof(struct Coeff));
+	M->coeffs = (struct Coeff *)malloc(M->nnz * sizeof(struct Coeff));
+
+	for (int i = 0; i < m->tri_count; i++) {
+		int a = m->triangles[i].a;
+		int b = m->triangles[i].b;
+		int c = m->triangles[i].c;
+		assert(a < N);
+		assert(b < N);
+		assert(c < N);
+		struct Vertex A = m->vertices[a];
+		struct Vertex B = m->vertices[b];
+		struct Vertex C = m->vertices[c];
+
+		struct Vector AB = vector(A, B);
+		struct Vector BC = vector(B, C);
+		struct Vector CA = vector(C, A);
+
+		double area = 0.5 * norm(cross(CA, AB));
+
+		struct Coeff *mass = &M->coeffs[9 * i];
+		mass[0] = {a, a, area / 6};
+		mass[1] = {b, b, area / 6};
+		mass[2] = {c, c, area / 6};
+		mass[3] = {a, b, area / 12};
+		mass[4] = {b, a, area / 12};
+		mass[5] = {a, c, area / 12};
+		mass[6] = {c, a, area / 12};
+		mass[7] = {b, c, area / 12};
+		mass[8] = {c, b, area / 12};
+
+		struct Coeff *s = &S->coeffs[9 * i];
+		double r = 1. / (4 * area);
+		s[0] = {a, a, dot(BC, BC) * r};
+		s[1] = {b, b, dot(CA, CA) * r};
+		s[2] = {c, c, dot(AB, AB) * r};
+		s[3] = {a, b, dot(BC, CA) * r};
+		s[4] = {b, a, dot(BC, CA) * r};
+		s[5] = {a, c, dot(AB, BC) * r};
+		s[6] = {c, a, dot(AB, BC) * r};
+		s[7] = {b, c, dot(AB, CA) * r};
+		s[8] = {c, b, dot(AB, CA) * r};
+	}
+}
+
+/******************************************************************************
+ * Routines for elementary linear algebra in arbitrary (large) dimensions
+ *****************************************************************************/
+
+/* Create an (unitialized) array of N double precision floating point values */
+double *array(int N) { return (double *)malloc(N * sizeof(double)); }
+
+/* Vector product between two vectors in dim N */
+double blas_dot(const double *A, const double *B, int N)
+{
+	double ans = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		ans += A[i] * B[i];
+	}
+	return ans;
+}
+
+/* aX + bY -> Y  (axpby reads as aX plus bY)
+ * a and b are scalar, X and Y are vectors in dim N
+ */
+void blas_axpby(double a, const double *X, double b, double *Y, int N)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		Y[i] = a * X[i] + b * Y[i];
+	}
+}
+
+/******************************************************************************
+ * Solving AU=B where A is SPD of size NxN using steepest descent method.
+ * Find it back on a sheet of paper, not on Google !
+ * One minimizes the functional 1/2 <AU,U> - <B,U>.
+ * The minor peculiarity here is that A = S + M and we do not wish to
+ * add these two sparse matrices up-front but simply compute AU as SU + MU
+ * wherever needed.
+ *****************************************************************************/
+int gradient_system_solve(const struct SparseMatrix *S, const struct SparseMatrix *M, const double *B, double *U, int N)
+	{
+		for (int i = 0; i < N; ++i) {U[i] = 1};
+
+
+		double *r = array(N);
+		double *Ar = array(N);
+		double *Mr = array(N);
+		matrix_vector_product(S, U, r);
+		matrix_vector_product(M, U, Mr);
+		blas_axpby(1, Mr, 1, r, N);
+		blas_axpby(1, b, -1, r, N);
+
+		int iter = 0;
+		int itermax = 1000;
+		error2 = blas_dot(r, r, N);
+		double tol = 1e-6;
+		double tol2 = tol * tol;
+		while (error2 > tol2 && iter++ < itermax)
+		{
+			// calculate Ar
+			matrix_vector_product(S, U, Ar);
+			matrix_vector_product(M, U, Mr);
+			blas_axpby(1, Mr, 1, Ar, N);
+
+
+			double alpha = error2 / blas_dot(Ar, r, N);
+			// update U
+			blas_axpby(alpha, r, 1, U, N);
+
+			// update r
+			blas_axpby(-alpha, Ar, 1, r, N);
+		}
+		free(r);
+		free(Ar);
+		free(Mr);
+
+		
+	}
+
+/******************************************************************************
+ * Let's choose our right hand side f of -\Delta u + u = f
+ *****************************************************************************/
+double f(double x, double y, double z)
+{
+	(void)z; /* Avoids compiler warning about unused variable */
+	return x * x - y * y;
+}
+
+/******************************************************************************
+ * Mesh construction routine declared here, defined later below main routine.
+ *****************************************************************************/
+void build_cube_mesh(struct Mesh *m, int N);
+void send_cube_to_sphere(struct Vertex *vert, int vtx_count);
+/******************************************************************************
+ * Main routine
+ *****************************************************************************/
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		return EXIT_FAILURE;
+
+	struct Mesh m;
+	build_cube_mesh(&m, atoi(argv[1]));
+	send_cube_to_sphere(m.vertices, m.vtx_count);
+	int N = m.vtx_count;
+	printf("Number of DOF : %d\n", N);
+
+	struct SparseMatrix M;
+	struct SparseMatrix S;
+	build_fem_matrices(&m, &S, &M);
+
+	/* Fill F */
+	double *F = array(N);
+	for (int i = 0; i < N; i++) {
+		struct Vertex v = m.vertices[i];
+		F[i] = f(v.x, v.y, v.z);
+	}
+	/* Fill B = MF */
+	double *B = array(N);
+	matrix_vector_product(&M, F, B);
+
+	/* Solve (S + M)U = B */
+	double *U = array(N);
+	int iter = gradient_system_solve(&S, &M, B, U, N);
+	printf("System solved in %d iterations.\n", iter);
+
+	printf("Integrity check :\n");
+	printf("-----------------\n");
+	for (int i = 0; i < 8; i++) {
+		if (F[i] != 0) {
+			printf("Ratio U/F : %f\n", U[i] / F[i]);
+		}
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+/******************************************************************************
+ * Building a cube surface mesh. N is the number of subdivisions per side.
+ * ***************************************************************************/
+int build_cube_vertices(struct Vertex *vert, int N);
+
+int build_cube_triangles(struct Triangle *tri, int N);
+
+int dedup_mesh_vertices(struct Mesh *m);
+
+void build_cube_mesh(struct Mesh *m, int N)
+{
+	/* Number of vertices per side = number of divisions + 1 */
+	int V = N + 1;
+
+	/* We allocate for 6 * V^2 vertices */
+	int max_vert = 6 * V * V;
+	m->vertices = (struct Vertex *)malloc(max_vert * sizeof(struct Vertex));
+	m->vtx_count = 0;
+
+	/* We allocate for 12 * N^2 triangles */
+	int tri_count = 12 * N * N;
+	m->triangles =
+	    (struct Triangle *)malloc(tri_count * sizeof(struct Triangle));
+	m->tri_count = 0;
+
+	/* We fill the vertices and then the faces */
+	m->vtx_count = build_cube_vertices(m->vertices, N);
+	m->tri_count = build_cube_triangles(m->triangles, N);
+
+	/* We fix-up vertex duplication */
+	m->vtx_count = dedup_mesh_vertices(m);
+	assert(m->vtx_count == 6 * V * V - 12 * V + 8);
+
+	/* Rescale to unit cube centered at the origin */
+	for (int i = 0; i < m->vtx_count; ++i) {
+		struct Vertex *v = &m->vertices[i];
+		v->x = 2 * v->x / N - 1;
+		v->y = 2 * v->y / N - 1;
+		v->z = 2 * v->z / N - 1;
+	}
+}
+
+/******************************************************************************
+ * The so-called spherical cube, built by simply normalizing all vertices of
+ * the cube mesh so that they end up in S^2
+ *****************************************************************************/
+void send_cube_to_sphere(struct Vertex *vert, int vtx_count)
+{
+	for (int i = 0; i < vtx_count; i++) 
+	{
+		struct Vertex *v = &vert[i];
+		double norm = sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+		v->x /= norm;
+		v->y /= norm;
+		v->z /= norm;
+	}
+}
+/*****************************************************************************/
